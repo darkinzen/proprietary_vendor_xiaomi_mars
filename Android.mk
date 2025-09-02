56b56efe0ff5c81c49a488f59c93718930ce5d60
darkinzen <h.bilgic1818@gmail.com>
2025-09-03 02:33:34 +0300

    mars: Extracted from star OS1.0.11.0.UKAMIXM

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),mars)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,c66cb5da05993b9a9c1b0948450f0dd7f8cbd979)
+$(call add-radio-file-sha1-checked,radio/aop.img,17885472f516a077ad85af05fa75ca55b76818cd)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,c5c5c732c03baf76dd3d3300b298577f4e406f8d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,cd7786272cd9e027b1b8d64aef04829bac07b4f0)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,602e6a6a8716c1049299bdd0ab424512730ee672)
+$(call add-radio-file-sha1-checked,radio/dsp.img,453413b40e59bfbd20bcb03dce4e3538f0f04550)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,5bb6e81f3845ada12aaf4556a999f55ade3903e0)
+$(call add-radio-file-sha1-checked,radio/hyp.img,68ff510f63f4db52fe67c1cd798947ef7a9acda0)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,b6e6e9b4fc7f69971f1a3b847e476e6eb003f116)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,f4255d2dc0d2dcf5fe61ea21844a6292b7af8395)
+$(call add-radio-file-sha1-checked,radio/modem.img,9ec49ecc420a8c0cf4f8adc94e903c0ab027e3b2)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,65493a9adfaaf7bd4da9f2401b23fef03682f60c)
+$(call add-radio-file-sha1-checked,radio/shrm.img,607181e299c5f0b1bcb0c964dc43fa8a4a5208f4)
+$(call add-radio-file-sha1-checked,radio/tz.img,f758cc157d75f6b5884023a41238cf9cc60913bb)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,27777e848d8ecd6e78e55009d4151cad037596b8)
+$(call add-radio-file-sha1-checked,radio/xbl.img,5f5976317f9fce707a0ddd540beded347e434ca5)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,5709c3ca40671238086ca2cb4c5a2e27819c35c5)
+
+endif
